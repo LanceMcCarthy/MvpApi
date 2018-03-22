4353785d4e32612d772cae5dace150e0768546bc
Lance McCarthy <lance.mccarthy@telerik.com>
2018-03-21 20:12:20 -0400

    Added background task project and UpdateTask task

---
+++ MvpApi.UwpBackgroundTasks/UpdateTask.cs
@@ -0,0 +1,22 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace MvpApi.UwpBackgroundTasks
+{
+    public sealed class UpdateTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var toastnotifier = ToastNotificationManager.CreateToastNotifier();
+            var toastDescriptor = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+            var txtNodes = toastDescriptor.GetElementsByTagName("text");
+            txtNodes[0].AppendChild(toastDescriptor.CreateTextNode("Updated!"));
+            txtNodes[1].AppendChild(toastDescriptor.CreateTextNode("MVP Companion has been successfully updated with lots of improvements and fixes. Tap this notification to check it out."));
+
+            var toast = new ToastNotification(toastDescriptor);
+
+            toastnotifier.Show(toast);
+        }
+    }
+}
