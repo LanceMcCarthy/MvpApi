5dab3781e29f0ff1f52f129c748a9952e4d42451
Lance McCarthy <lance.mccarthy@progress.com>
2019-06-09 10:46:31 -0400

    Added self-elevation to PS scripts

--- helper_scripts/Find-WindowsSDKVersions.ps1
+++ helper_scripts/Find-WindowsSDKVersions.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 $script:ns = 'http://schemas.microsoft.com/developer/msbuild/2003'
 
 $ErrorActionPreference = 'Stop'
--- helper_scripts/Install-WindowsSDK.ps1
+++ helper_scripts/Install-WindowsSDK.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 mkdir c:\winsdktemp
 
 $client = new-object System.Net.WebClient
