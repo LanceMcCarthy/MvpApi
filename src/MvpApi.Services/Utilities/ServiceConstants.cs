6b329dc77468c47f25a91475c23206c942755656
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-03-23 18:19:34 -0400

    Bumped contribution submission window for 2020-2022 time

--- src/MvpApi.Services/Utilities/ServiceConstants.cs
+++ src/MvpApi.Services/Utilities/ServiceConstants.cs
@@ -4,8 +4,8 @@ namespace MvpApi.Services.Utilities
 {
     public static class ServiceConstants
     {
-        public static DateTime SubmissionStartDate { get; set; } = new DateTime(2016, 10, 1);
+        public static DateTime SubmissionStartDate { get; set; } = new DateTime(2020, 6, 1);
 
-        public static DateTime SubmissionDeadline { get; set; } = new DateTime(2020, 4, 1);
+        public static DateTime SubmissionDeadline { get; set; } = new DateTime(2022, 4, 1);
     }
 }
