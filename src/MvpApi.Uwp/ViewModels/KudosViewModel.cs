100ada8d866243d97775a5d011be91d934e934b5
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-02-16 15:29:56 -0500

    [uwp] Moved Store review functionality from Kudos page to Shell nav menu

--- src/MvpApi.Uwp/ViewModels/KudosViewModel.cs
+++ src/MvpApi.Uwp/ViewModels/KudosViewModel.cs
@@ -23,7 +23,7 @@ namespace MvpApi.Uwp.ViewModels
 
         public KudosViewModel()
         {
-            KudosCollection.Add(new Kudos { Title = "Store Rating", Price = "Free", ImageUrl = "/Images/4starStar.png" });
+            //KudosCollection.Add(new Kudos { Title = "Store Rating", Price = "Free", ImageUrl = "/Images/4starStar.png" });
             KudosCollection.Add(new Kudos { Title = "Small Coffee", ProductId = "MvpCompanion_SmallCoffee", StoreId = "9NJ9NKHQF7C4", Price = "$1.49", ImageUrl = "/Images/CoffeeKudo.png" });
             KudosCollection.Add(new Kudos { Title = "Lunch", ProductId = "MvpCompanion_Lunch", StoreId = "9N999Z3H3GPK", Price = "$4.89", ImageUrl = "/Images/LunchKudo.png" });
             KudosCollection.Add(new Kudos { Title = "Dinner", ProductId = "MvpCompanion_Dinner", StoreId = "9NB8SR731DM6", Price = "$9.49", ImageUrl = "/Images/DinnerKudo.png" });
@@ -49,64 +49,13 @@ namespace MvpApi.Uwp.ViewModels
                 await PurchaseKudosAsync(kudo.StoreId);
             }
 
-            if (kudo.Title == "Store Rating")
-            {
-                await ShowRatingReviewDialog();
-            }
+            //if (kudo.Title == "Store Rating")
+            //{
+            //    await ShowRatingReviewDialog();
+            //}
         }
 
-        public async Task ShowRatingReviewDialog()
-        {
-            try
-            {
-                IsBusy = true;
-                IsBusyMessage = "rating and review in progress (you should see a separate window)...";
-
-                var result = await StoreRequestHelper.SendRequestAsync(StoreContext.GetDefault(), 16, "");
-
-                IsBusyMessage = "action complete, reviewing result...";
-
-                if (result.ExtendedError != null)
-                    return;
-
-                var jsonObject = JObject.Parse(result.Response);
-                var status = jsonObject.SelectToken("status").ToString();
-
-                IsBusyMessage = "action complete, showing result...";
-
-                if (status == "success")
-                {
-                    await new MessageDialog("Thank you for taking the time to leave a rating! If you left 3 stars or lower, please let me know how I can improve the app (go to About page).", "Success").ShowAsync();
-                }
-                else if (status == "aborted")
-                {
-                    var md = new MessageDialog("If you prefer not to leave a bad rating but still want to provide feedback, click the email button below. I work hard to make sure you have a great app experience and would love to hear from you.", "Review Aborted");
-
-                    md.Commands.Add(new UICommand("send email"));
-                    md.Commands.Add(new UICommand("not now"));
-
-                    var mdResult = await md.ShowAsync();
-
-                    if (mdResult.Label == "send email")
-                    {
-                        await FeedbackHelpers.Current.EmailFeedbackMessageAsync();
-                    }
-                }
-                else
-                {
-                    await new MessageDialog($"The rating or review did not complete, here's what Windows had to say: {jsonObject.SelectToken("status")}.\r\n\nIf you meant to leave a review, try again. If this keeps happening, contact us and share the error code above.", "Rating or Review was not successful").ShowAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                await ex.LogExceptionWithUserMessage();
-            }
-            finally
-            {
-                IsBusy = false;
-                IsBusyMessage = "";
-            }
-        }
+        
 
         public async Task PurchaseKudosAsync(string storeId)
         {
