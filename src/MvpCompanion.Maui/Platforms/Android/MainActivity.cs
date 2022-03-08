9a356bcbeacb3007eed86cd2d142f2b27627f1fe
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-03-08 17:21:54 -0500

    [maui] More work on notification service, removed webauth system (wont work for MVP api)

--- src/MvpCompanion.Maui/Platforms/Android/MainActivity.cs
+++ src/MvpCompanion.Maui/Platforms/Android/MainActivity.cs
@@ -2,13 +2,11 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using Android.Widget;
-using Microsoft.Maui;
 
 namespace MvpCompanion.Maui;
 
-[IntentFilter(new[] { Platform.Intent.ActionAppAction }, Categories = new[] { Android.Content.Intent.CategoryDefault })]
+[IntentFilter(new[] { Platform.Intent.ActionAppAction }, Categories = new[] { Intent.CategoryDefault })]
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
@@ -16,45 +14,41 @@ public class MainActivity : MauiAppCompatActivity
     {
         base.OnCreate(savedInstanceState);
         
-        Microsoft.Maui.Essentials.Platform.Init(this, savedInstanceState);
+        Platform.Init(this, savedInstanceState);
 
-        Microsoft.Maui.Essentials.Platform.ActivityStateChanged += Platform_ActivityStateChanged;
+        Platform.ActivityStateChanged += Platform_ActivityStateChanged;
     }
 
     protected override void OnResume()
     {
         base.OnResume();
 
-        Microsoft.Maui.Essentials.Platform.OnResume(this);
+        Platform.OnResume(this);
     }
 
-    protected override void OnNewIntent(Android.Content.Intent intent)
+    protected override void OnNewIntent(Intent intent)
     {
         base.OnNewIntent(intent);
 
-        Microsoft.Maui.Essentials.Platform.OnNewIntent(intent);
+        Platform.OnNewIntent(intent);
     }
+
     protected override void OnDestroy()
     {
         base.OnDestroy();
 
-        Microsoft.Maui.Essentials.Platform.ActivityStateChanged -= Platform_ActivityStateChanged;
+        Platform.ActivityStateChanged -= Platform_ActivityStateChanged;
     }
 
-    void Platform_ActivityStateChanged(object sender, Microsoft.Maui.Essentials.ActivityStateChangedEventArgs e) =>
-        Toast.MakeText(this, e.State.ToString(), ToastLength.Short).Show();
+    void Platform_ActivityStateChanged(object sender, ActivityStateChangedEventArgs e)
+    {
+        Toast.MakeText(this, e.State.ToString(), ToastLength.Short)?.Show();
+    }
 
     public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
     {
-        Microsoft.Maui.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
         base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
     }
-}
-
-// See https://docs.microsoft.com/en-us/xamarin/essentials/web-authenticator?context=xamarin%2Fxamarin-forms&tabs=android
-[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
-[IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataScheme = "xamarinessentials")]
-public class WebAuthenticationCallbackActivity : Microsoft.Maui.Essentials.WebAuthenticatorCallbackActivity
-{
 }
---
+++ src/MvpCompanion.Maui/Platforms/Android/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿using MvpCompanion.Maui.Services;
+using Android.Widget;
+using Android.Content;
+
+namespace MvpCompanion.Maui
+{
+    public class NotificationService : INotificationService
+    {
+        public void ShowNotification(string title, string body)
+        {
+            Toast.MakeText(Android.App.Application.Context, body, ToastLength.Long)?.Show();
+        }
+    }
+}
