bb51c5e7ef78a7be5f332adc668bcedbd63ae852
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-03-21 16:21:30 -0400

    [maui] added some platform view code to WnUI app for future use

--- src/MvpCompanion.Maui/Platforms/Windows/App.xaml.cs
+++ src/MvpCompanion.Maui/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
 
 namespace MvpCompanion.Maui.WinUI;
 
@@ -7,6 +9,22 @@ public partial class App : MauiWinUIApplication
     public App()
     {
         this.InitializeComponent();
+
+        Microsoft.Maui.Handlers.WindowHandler.ElementMapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            // Native WinUI app
+            var nativeApp = handler.PlatformView as MvpCompanion.Maui.WinUI.App;
+
+            // Maui app
+            var app = view as MvpCompanion.Maui.App;
+
+            //nativeWindow.Activate();
+
+            //IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            //WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            //AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            //appWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 500, 500));
+        });
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
