02e2b5a95b3f802a4c0add2e54c7f153971e65a3
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-12-11 10:54:43 -0500

    [XF] Delete Xamarin.Forms projects

--- src/MvpApi.Forms/Portable/Common/INavigationHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using MvpApi.Forms.Portable.Models;
-
-namespace MvpApi.Forms.Portable.Common
-{
-    public interface INavigationHandler
-    {
-        void LoadView(ViewType viewType);
-    }
-}
